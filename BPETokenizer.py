bcff58ae132121c9e3834d821738f39519dfe9b5
jessherlitz <jesssherlitz@gmail.com>
2026-03-11 17:08:58 -0400

    add vocab building and merge tracking to training loop

--- BPETokenizer.py
+++ BPETokenizer.py
@@ -6,8 +6,10 @@ class BPETokenizer:
 		self.vocab: dict[str, int] = {}
 		self.word_freq: defaultdict[str, int] = defaultdict(int)
 		self.splits: defaultdict[tuple[str, ...], int] = defaultdict(int)
+		self.merges: list[tuple[str, str]] = []
 		self._build_word_freqs()
 		self._build_char_splits()
+		self._build_initial_vocab()
 
 
 	def _clean_text(self, text: str) -> str:
@@ -26,7 +28,6 @@ class BPETokenizer:
 			w[-1] = w[-1] + '</w>'
 			self.splits[tuple(w)] = freq
 
-
 	def _count_pairs(self) -> dict[tuple, int]:
 		pairs_count: defaultdict[tuple[str, str], int] = defaultdict(int)
 
@@ -43,7 +44,9 @@ class BPETokenizer:
 			if not pairs_count:
 				break
 			best_pair = max(pairs_count, key=pairs_count.get)
+			self.merges.append(best_pair)
 			self._merge(best_pair)
+			self.vocab[best_pair[0] + best_pair[1]] = len(self.vocab)
 
 	def _merge(self, pair: tuple[str, str]) -> None:
 		new_splits: defaultdict[tuple[str, ...], int] = defaultdict(int)
@@ -60,6 +63,17 @@ class BPETokenizer:
 					i += 1
 			new_splits[tuple(new_tokens)] = freq
 		self.splits = new_splits
+
+
+	def _build_initial_vocab(self) -> None:
+		token_id: int = 0
+		for entry in self.splits.keys():
+			for token in entry:
+				if token not in self.vocab:
+					self.vocab[token] = token_id
+					token_id += 1
+
+
 
 
 
